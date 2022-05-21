45dac72f8cb7a28750a403e44d7d5282277aff91
Osebest <udazi.osebest@gmail.com>
2022-05-21 21:40:11 +0100

    Trying to Deploy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Hompage from './components/Hompage';
 import Calculator from './components/Calculator';
